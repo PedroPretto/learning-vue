9c78d14238bf0b59449c88a35f485b5f597740b4
PedroPretto <pedropretto18@gmail.com>
2021-03-25 23:54:25 -0300

    first project

---
+++ first-project/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>Filter Learnvue Articles</h2>
+    <input type="text" placeholder="Filter Search" v-model="query"/>
+    <button @click="reset">Reset</button>
+    <br>
+    {{query}}
+    <search-results :query='query'/>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import SearchResults from './SearchResults.vue'
+export default {
+  components: {
+    SearchResults
+  },
+  setup () {
+    const query = ref('')
+
+    const reset = () => {
+      query.value = ''
+    }
+    return{
+      query,
+      reset,
+      SearchResults
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
